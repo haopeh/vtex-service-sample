3727e360a375d87c77d1061f2544fa633f754317
zhenjiazhou <zhenjia.zhou@thoughtworks.com>
2023-06-28 15:30:12 +0800

    bugfix:fix shipping  and payment data bug

--- node/middlewares/checkout.ts
+++ node/middlewares/checkout.ts
@@ -1,4 +1,5 @@
 import { json } from 'co-body'
+// import { readFileSync } from "fs";
 
 export async function getOrCreateCart(ctx: Context, next: () => Promise<any>) {
   const {
@@ -31,7 +32,7 @@ export async function getCartPage(ctx: Context, next: () => Promise<any>) {
   const id = formOrderId as string
 
   ctx.body = await checkoutClient.getAllOrdersCart(id)
-
+  console.info('ctx.body', ctx.body)
   await next()
 }
 
@@ -63,9 +64,12 @@ export async function addCartItems(ctx: Context, next: () => Promise<void>) {
   const currentForm = await checkoutClient.getAllOrdersCart(formOrderId)
   const currentItems = currentForm.items
 
+  console.info('currentItems', currentItems)
+  console.info('orderItem request', orderItem)
   const updatedItem = updateItemQuantity(currentItems, orderItem)
   const vtexResponse = await checkoutClient.addItem(formOrderId, updatedItem)
 
+  console.info('vtexResponse', vtexResponse)
   ctx.body = vtexResponse.items
   ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
   ctx.set('Access-Control-Allow-Origin', '*')
@@ -107,6 +111,7 @@ export async function addShippingData(ctx: Context, next: () => Promise<void>) {
   const { formOrderId } = params
 
   console.info('order id', formOrderId)
+  console.info(body)
 
   ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
   ctx.set('Access-Control-Allow-Origin', '*')
@@ -133,6 +138,7 @@ export async function addPaymentData(ctx: Context, next: () => Promise<void>) {
   ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
   ctx.set('Access-Control-Allow-Origin', '*')
   ctx.body = await checkoutClient.addPaymentData(formOrderId, body)
-  console.info(ctx.body)
+  // since the vtex api returns an empty body, we need to return the body we want
+  // ctx.body.paymentData=readFileSync()
   await next()
 }
